6b2d0299f2ce9db7f1ae0f158c9c14a22e181e87
Supopur <matylda.smyk.09@gmail.com>
2023-06-06 17:25:52 +0200

    broke the code reverting later

--- Main.cs
+++ Main.cs
@@ -14,8 +14,13 @@ namespace Code_Blue_Calls
     {
         public override void Initialize()
         {
+            // Subscribe to the OnOnDutyStateChanged event
             Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+
+            // Log a message to indicate that the callouts have been initialized
             Game.LogTrivial("Code Blue callouts initialized.");
+
+            // Register an event handler for resolving LSPDFR-related assemblies
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LSPDFRResolveEventHandler);
         }
 
@@ -24,46 +29,61 @@ namespace Code_Blue_Calls
             throw new NotImplementedException();
         }
 
+        // Event handler for the OnOnDutyStateChanged event
         public static void OnOnDutyStateChangedHandler(bool OnDuty)
         {
             if (OnDuty)
             {
-                RegisterCallouts(); 
+                // If the player goes on duty, register the callouts
+                RegisterCallouts();
+
+                // Display a notification to indicate that the callouts have been loaded
                 Game.DisplayNotification("Code Blue Callouts loaded.");
             }
         }
 
+        // Register the callouts
         private static void RegisterCallouts()
         {
             Functions.RegisterCallout(typeof(Callouts.DomesticViolence));
         }
 
+        // Event handler for resolving LSPDFR-related assemblies
+        // I stole this code from a tutorial i have no idea how it works but i just know that it checks for lspdfr version. Btw comments are made by chat gpt
         public static Assembly LSPDFRResolveEventHandler(object sender, ResolveEventArgs args)
         {
+            // Iterate through all user plugins to find the requested assembly
             foreach (Assembly assembly in Functions.GetAllUserPlugins())
             {
+                // Check if the assembly name matches the requested name
                 if (args.Name.ToLower().Contains(assembly.GetName().Name.ToLower()))
                 {
-                    return assembly; 
+                    return assembly; // Return the assembly if found
                 }
             }
-            return null;
+            return null; // Return null if the assembly is not found
         }
 
+        // Check if an LSPDFR plugin is running
+        // I stole this code from a tutorial i have no idea how it works but i just know that it checks for lspdfr version. Btw comments are made by chat gpt
         public static bool IsLSPDFRPluginRunning(string Plugin, Version minversion = null)
         {
+            
             foreach (Assembly assembly in Functions.GetAllUserPlugins())
             {
                 AssemblyName an = assembly.GetName();
+
+                // Check if the plugin name matches the requested name
                 if (an.Name.ToLower() == Plugin.ToLower())
                 {
+                    // Check the version if a minimum version is provided
                     if (minversion == null || an.Version.CompareTo(minversion) >= 0)
                     {
-                        return true;
+                        return true; // Return true if the plugin is running and meets the minimum version requirement
                     }
                 }
             }
-            return false;
+            return false; // Return false if the plugin is not found or does not meet the minimum version requirement
         }
     }
 }
